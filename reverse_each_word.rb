6c94e6c9089a5165a96ee6891a65160dbf46c75a
Robin Kim <rkim1214@gmail.com>
2018-10-17 15:35:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,11 @@ def reverse_each_word(string)
   end 
   reversed_array.join(" ")
 end 
+
+def reverse_each_word(string)
+  array = string.split
+  array.collect do |word|
+    word.reverse 
+  end 
+  array.join(" ")
+end 
