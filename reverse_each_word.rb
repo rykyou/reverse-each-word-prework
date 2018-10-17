7c061b1eb9ac972540ff7dd6a9c6d3584a3bd069
Robin Kim <rkim1214@gmail.com>
2018-10-17 15:32:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(string)
   array = string.split
+  reversed_array = []
   array.each do |word|
-    word.reverse 
+    reversed_array << word.reverse 
+  end 
+  reversed_array.join
+end 
